aa31c704f26e3af3eb20bccc23e6ba14134e5f6e
Steven Lin <steven_lx@outlook.com>
2020-02-11 11:16:23 -0800

    initial check-in

---
+++ DddInPractice.Logic/Common/DomainEvents.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DddInPractice.Logic.Common
+{
+   
+    public static class DomainEvents
+    {
+        private static List<Type> _handlers;
+
+
+    }
+}
